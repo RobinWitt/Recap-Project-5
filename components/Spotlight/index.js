15410504ba5fad824b458a1b1f8a260db75c7a9d
Robin Witt <robin.w@posteo.de>
2023-01-09 14:27:54 +0100

    implement test

--- components/Spotlight/index.js
+++ components/Spotlight/index.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import getRandomNumber from "./GetRandomNumber";
+
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
 
 export default function Spotlight({ data }) {
   const randomImage = data[getRandomNumber(0, data.length - 1)];
